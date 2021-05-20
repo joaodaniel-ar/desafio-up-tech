d1e53fb5c7b2fd846c5a4bc56b03a9102d288e91
João Daniel <joaodaniel.tn@gmail.com>
2021-05-20 01:48:17 -0300

    Link Google Books API

---
+++ src/BookCard.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { Card, CardImg, Modal } from 'reactstrap';
+const BookCard = ({
+  thumbnail,
+  title,
+  pageCount,
+  language,
+  description,
+  authors,
+  publisher,
+  previewLink,
+  infoLink
+}) => {
+  // States
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  return (
+    <Card style={{ width: '200px' }} className='m-auto '>
+      <CardImg
+        top
+        style={{ width: '100%', height:'270px' }}
+        src={thumbnail}
+        alt={title}
+        onClick={toggle}
+      />
+      <Modal isOpen={modal} toggle={toggle}>
+        <div className='modal-header d-flex justify-content-center'>
+          <h5 className='modal-title text-center' id='exampleModalLabel'>
+            {title}
+          </h5>
+          <button
+            aria-label='Close'
+            className='close'
+            type='button'
+            onClick={toggle}
+          >
+            <span aria-hidden={true}>X</span>
+          </button>
+        </div>
+        <div className='modal-body'>
+          <div className='d-flex justify-content-between ml-3'>
+            <img src={thumbnail} alt={title} style={{ height: '320px' }} />
+            <div class="book-content">
+              <p>{description}</p>
+              <p>Page Count: {pageCount} | Language : {language} | Authors : {authors} | Publisher : {publisher}</p>
+            </div>
+          </div>
+          <div>
+          </div>
+        </div>
+        <div className='modal-footer'>
+          <div className='left-silde'>
+            <a
+              href={previewLink}
+              className='btn-link'
+              color='default'
+              type='button'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Preview Link
+            </a>
+          </div>
+          <div className='divider'></div>
+          <div className='right-silde'>
+            <a
+              href={infoLink}
+              className='btn-link'
+              color='default'
+              type='button'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Info Link
+            </a>
+          </div>
+        </div>
+      </Modal>
+    </Card>
+  );
+};
+
+export default BookCard;
